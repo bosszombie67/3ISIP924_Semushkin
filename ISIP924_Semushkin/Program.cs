859a8f9e402c6eb0b286ea4e95fccfcbe84739db
Doctor <g.semushkin@gmail.com>
2026-09-09 10:49:20 +0300

    Сделал начальный дизайн и вывод, добавил проверку на ввод нужного кол-ва операций

--- ISIP924_Semushkin/Program.cs
+++ ISIP924_Semushkin/Program.cs
@@ -10,6 +10,48 @@ namespace ISIP924_Semushkin
     {
         static void Main(string[] args)
         {
+            
+            int choice = 0, count = 0;
+            string trata;
+            Console.WriteLine("Введите количество операций, которые будут записаны:");
+            count = int.Parse(Console.ReadLine());
+            if (count < 2 && count > 40)
+            {
+                Console.WriteLine("Вы ввели либо меньше двух, либо больше сорока операций");
+            }
+            else
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    Console.WriteLine("Введите ваши траты в рублях, в формате(Название услуги или товара; Количество денег):");
+                    trata = Console.ReadLine();
+                    Console.WriteLine("1. Вывод данных");
+                    Console.WriteLine("2. Статистика (среднее, максимальное, минимальное, сумма)");
+                    Console.WriteLine("3. Сортировка по цене (пузырьковая сортировка)");
+                    Console.WriteLine("4. Конвертация валюты (пользователь вводит курс или выбирает из списка)");
+                    Console.WriteLine("5. Поиск по названию ");
+                    Console.WriteLine("0. Выход");
+                    Console.WriteLine("Ваш выбор операции:");
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
     }
 }
