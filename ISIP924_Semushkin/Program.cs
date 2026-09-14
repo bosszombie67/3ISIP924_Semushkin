b480610cb4448ebeaf9a31bce5e006371806bea4
bosszombie67 <gptsinok@gmail.com>
2026-09-14 19:56:45 +0300

    Создал класс для товара и перечисление для категории товаров

--- ISIP924_Semushkin/Program.cs
+++ ISIP924_Semushkin/Program.cs
@@ -10,6 +10,27 @@ namespace ISIP924_Semushkin
     {
         static void Main(string[] args)
         {
+            public enum Category
+        {
+            Peripherals,
+            Computer Hardware,
+            Audio/Video Equipment,
+            Consoles
+        }
+            class Goods
+        {
+            private int uCode;
+            public int nextCode = 1;
+            private string name { get; set; }
+            private decimal price { get; set; }
+            private int quantity { get;  set; }
+            private bool state { get; set; }
+            private string category { get; set; }
+            public Product()
+            {
+
+            }
+        }
         }
     }
 }
