f09317b7f762538a23b01914134678bc7f325f5e
bosszombie67 <gptsinok@gmail.com>
2026-09-16 10:28:51 +0300

    Исправил ошибки в классе

--- ISIP924_Semushkin/Program.cs
+++ ISIP924_Semushkin/Program.cs
@@ -8,29 +8,35 @@ namespace ISIP924_Semushkin
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            public enum Category
+        public enum Category
         {
             Peripherals,
-            Computer Hardware,
-            Audio/Video Equipment,
+            Computer_Hardware,
+            Audio_Video_Equipment,
             Consoles
         }
-            class Goods
+        class Goods
         {
             private int uCode;
-            public int nextCode = 1;
-            private string name { get; set; }
-            private decimal price { get; set; }
-            private int quantity { get;  set; }
-            private bool state { get; set; }
-            private string category { get; set; }
-            public Product()
+            public int NextCode = 1;
+            private string Name { get; set; }
+            private decimal Price { get; set; }
+            private int Quantity { get; set; }
+            private bool State => Quantity > 0;
+            private Category Category { get; set; }
+            public Goods(string name, decimal price, int quantity, Category category)
             {
-
+                uCode = NextCode;
+                NextCode++;
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+                Category = category;
             }
         }
+        static void Main(string[] args)
+        {
+
         }
     }
 }
