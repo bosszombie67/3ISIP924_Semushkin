69f3bfa2984af5751a8d5b25a99e76b669187622
bosszombie67 <gptsinok@gmail.com>
2026-09-25 10:23:57 +0300

    Реализовал функцию удаления товара по коду, добавил clear для красивого вывода в консоли

--- ISIP924_Semushkin/Program.cs
+++ ISIP924_Semushkin/Program.cs
@@ -8,17 +8,10 @@ namespace ISIP924_Semushkin
 {
     internal class Program
     {
-        public enum Category
-        {
-            Peripherals = 1,
-            Computer_Hardware = 2,
-            Audio_Video_Equipment = 3,
-            Consoles = 4
-        }
         class Goods
         {
             private int uCode { get; }
-            public int NextCode = 1;
+            public static int NextCode = 1;
             private string Name { get; set; }
             private decimal Price { get; set; }
             private int Quantity { get; set; }
@@ -32,58 +25,89 @@ namespace ISIP924_Semushkin
                 Quantity = quantity;
                 Category = category;
             }
-            static void AddGoods(List<Goods> Products){
+            public static void AddGoods(List<Goods> Products)
+            {
+                Console.Clear();
                 Console.Write("Введите название товара: ");
                 string name = Console.ReadLine();
                 Console.Write("Введите цену товара: ");
                 decimal price = decimal.Parse(Console.ReadLine());
                 Console.Write("Введите количество товара: ");
                 int quantity = int.Parse(Console.ReadLine());
-                Console.Write("Выберите категорию товара: ");
                 Category category = ChooseCategory();
                 Goods newGoods = new Goods(name, price, quantity, category);
                 Products.Add(newGoods);
+                Console.WriteLine("Товар успешно добавлен");
+                Console.Clear();
             }
-            static void DeleteGoods(List<Goods> Products)
+            static Category ChooseCategory()
             {
-
-            }
-            static void ShowGoods(List<Goods> Products)
-            {
-
-            }
-            static void FindGoods(List<Goods> Products)
-            {
-
-            }
-            static void SellGoods(List<Goods> Products)
-            {
-
-            }
-        }
-        static Category ChooseCategory()
-        {
-            while (true)
-            {
-                Console.WriteLine("Категории:");
-                Console.WriteLine("1. Периферийные устройства");
-                Console.WriteLine("2. Компьютерное оборудование");
-                Console.WriteLine("3. Аудио/Видео оборудование");
-                Console.WriteLine("4. Консоли");
-                Console.Write("Выберите категорию: ");
-                if (int.TryParse(Console.ReadLine(), out int number))
+                Console.Clear();
+                while (true)
                 {
+                    Console.WriteLine("Категории:");
+                    Console.WriteLine("1. Периферийные устройства");
+                    Console.WriteLine("2. Компьютерное оборудование");
+                    Console.WriteLine("3. Аудио/Видео оборудование");
+                    Console.WriteLine("4. Консоли");
+                    Console.Write("Выберите категорию: ");
+                    if (int.TryParse(Console.ReadLine(), out int number))
+                    {
                         if (number >= 1 && number <= 4)
                         {
-                            return (Category)(number - 1);
+                            return (Category)number;
                         }
+                    }
+                    Console.WriteLine("Вы выбрали некорректную категорию");
+                    Console.Clear();
                 }
-                Console.WriteLine("Вы выбрали некорректную категорию");
+            }
+            public static void DeleteGoods(List<Goods> Products)
+            {
+                Console.Clear();
+                Console.Write("Введите код товара для удаления: ");
+                if (int.TryParse(Console.ReadLine(), out int code))
+                {
+                    for (int i = 0; i < Products.Count; i++)
+                    {
+                        if (Products[i].uCode == code)
+                        {
+                            Products.RemoveAt(i);
+                            Console.WriteLine("Товар удален.");
+                            Console.Clear();
+                            return;
+                        }
+                    }
+                }
+                Console.WriteLine("Товар не найден.");
+                Console.Clear();
+            }
+            public static void ShowGoods(List<Goods> Products)
+            {
+
+            }
+            public static void FindGoods(List<Goods> Products)
+            {
+
+            }
+            public static void SellGoods(List<Goods> Products)
+            {
+
             }
         }
+        public enum Category
+        {
+            Peripherals = 1,
+            Computer_Hardware = 2,
+            Audio_Video_Equipment = 3,
+            Consoles = 4
+        }
+
         static void Main(string[] args)
         {
             List<Goods> Products = new List<Goods>();
+            Goods.AddGoods(Products);
+            Goods.DeleteGoods(Products);
         }
     }
 }
